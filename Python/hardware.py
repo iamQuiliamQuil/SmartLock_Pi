0edcfdb4fef0fb4eb4861fcca29082ded8c3bc0f
iamQuiliamQuil <quillanbrady@yahoo.com>
2022-04-29 14:27:41 -0400

    lock state and delete captures

--- Python/hardware.py
+++ Python/hardware.py
@@ -6,12 +6,48 @@ from adafruit_motor import stepper
 from picamera import PiCamera
 from io import BytesIO
 from os import listdir
-from os.path import join
+from os import path
+from os import remove
+from distutils.util import strtobool
 
 #local files
 import sms
 
-storagePath = "/home/pi/Desktop/SmartLock_Pi/Storage/"
+storagePath = "/home/pi/Desktop/SmartLock_Pi/Storage/Captures/"
+
+flagsPath = "/home/pi/Desktop/SmartLock_Pi/Python/flags.txt"
+if not path.isfile(flagsPath):
+    with open(flagsPath, "w") as file:
+        file.write("locked=False")
+
+def __getFlags():
+    flags = {}
+    with open(flagsPath) as file:
+        flagStrings = file.read().split("\n")
+        for flagString in flagStrings:
+            flagData = flagString.split("=")
+            flags[flagData[0]] = flagData[1]
+    return flags
+
+def __dictToStr(dictionary):
+    dictStr = ""
+    for key, value in dictionary.items():
+        dictStr += key + "=" + value + "\n"
+    dictStr = dictStr[:-1]
+    return dictStr
+
+def __setFlag(flagName, flagValue):
+    flags = __getFlags()
+    flags[flagName] = str(flagValue)
+    with open(flagsPath, "w") as file:
+        file.write(__dictToStr(flags))
+
+def __getFlag(flagName):
+    flags = __getFlags()
+    return bool(strtobool(flags[flagName]))
+
+def __checkFlag(flagName, flagValue):
+    return flagValue == __getFlag(flagName)
 
 def handle(args):
     arguments = args.split("_", 1)
@@ -38,30 +74,48 @@ def capture(resolution, filetype):
     filename = strftime("%d-%m-%y-%H-%M-%S." + filetype, gmtime())
     camera.capture(storagePath + filename, filetype)
     camera.close()
-    sms.send("15186505491","a picture was captured!")
+    #sms.send("15186505491", "Image Captured!\n" + filename)
     return filename
 
 def getCapture(filename):
     imgBytes = BytesIO()
     filepath = storagePath + filename
+    filetype = path.splitext(filename)[1][1:]
     print(filepath)
     with Image.open(filepath) as img:
-        img.save(imgBytes, "png")
+        img.save(imgBytes, filetype)
     imgBytes.seek(0) # start of bytes
     return imgBytes.read()
 
+def deleteCapture(filename):
+    filepath = storagePath + filename
+    remove(filepath)
+    return filename + " was deleted"
+
 def getPictureNames():
     #sends back all png filenames joined by ~'s as one string
-    return "~".join(sorted([file for file in listdir("../Storage") if ".png" in file],reverse=True))
+    return "~".join(sorted(listdir(storagePath), reverse=True))
 
 def lock():
-    kit = MotorKit()
-    for i in range(100):
-        kit.stepper1.onestep(direction=stepper.FORWARD)
-    return "locked"
+    if __checkFlag("locked", False):
+        kit = MotorKit()
+        for i in range(100):
+            kit.stepper1.onestep(direction=stepper.FORWARD)
+        __setFlag("locked", True)
+        return "locked"
+    return "did not lock"
 
 def unlock():
-    kit = MotorKit()
-    for i in range(100):
-        kit.stepper1.onestep(direction=stepper.BACKWARD)
-    return "unlocked"
+    if __checkFlag("locked", True):
+        kit = MotorKit()
+        for i in range(100):
+            kit.stepper1.onestep(direction=stepper.BACKWARD)
+        __setFlag("locked", False)
+        return "unlocked"
+    return "did not unlock"
+
+def getState():
+    return str(__getFlag("locked"))
+
+#print(getPictureNames())
+#print(getCapture("29-04-22-16-15-22.png"))
--- Python/test.py
+++ Python/test.py
@@ -2,6 +2,16 @@
 print(result)
 print(type(result))
 print(type(result) is bytes)"""
-
+import UUIDManager as um
 import hardware
-print(hardware.getPictureNames())
+import postRequestHandler as prh
+#print(hardware.getPictureNames())
+#hardware.capture("100x100","png")
+
+uuids = um.UUIDManagerSingleton()
+uuid = uuids.generateUUID()
+
+request = "uuuid~"+uuid+"Qrequest~lock"
+print("helooooo")
+
+prh.handleRequest(request)
