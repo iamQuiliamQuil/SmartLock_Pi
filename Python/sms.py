7b9f0e503ba8524892f0e472ddf148328241dd38
iamQuiliamQuil <qb666256>
2022-05-02 16:34:30 -0400

    implemented sms with picture

--- Python/sms.py
+++ Python/sms.py
@@ -3,4 +3,7 @@ import subprocess
 def send(phone_num, message):
     subprocess.check_call(['./sms.sh', phone_num, message])
     #TODO: parse message output and figure out if it succeeded or not
-    
+
+def send_image(phone_num, image_filename, ip_addr, message_body):
+    #message body is optional, send "" as a 4th argument if you don't want a message
+    subprocess.check_call(['./sms_picture.sh', phone_num, image_filename, ip_addr, message_body])
--- Python/sms_picture.sh
+++ Python/sms_picture.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-export TWILIO_ACCOUNT_SID=AC5bea98d360df7e4d50af0fb6d8e34621
-export TWILIO_AUTH_TOKEN=284691e649e31c7afeae5fb9694d481d
-export TWILIO_NUMBER=19804093427
-export TO_NUMBER=$1
-
-#curl -X POST -d "Body=$2" -d "From=$TWILIO_NUMBER" -d "To=$TO_NUMBER" "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages" -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
 
 curl -XPOST https://api.twilio.com/2010-04-01/Accounts/AC5bea98d360df7e4d50af0fb6d8e34621/Messages.json \
 --data-urlencode "To=$1" \
 --data-urlencode "From=+19804093427" \
---data-urlencode "MediaUrl=http://169.226.103.172:8001/Pictures/$2" \ 
---data-urlencode "Body=Hello from my Twilio line!" \
+--data-urlencode "MediaUrl=http://$3:8001/Pictures/$2" \
+--data-urlencode "Body=$4" \
 -u 'AC5bea98d360df7e4d50af0fb6d8e34621:284691e649e31c7afeae5fb9694d481d'
