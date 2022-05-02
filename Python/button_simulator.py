1f6990e1818cb1e296946d7578aa34bbc8b7dc5f
iamQuiliamQuil <qb666256>
2022-05-02 17:03:00 -0400

    added doorbell

--- Python/button_simulator.py
+++ Python/button_simulator.py
@@ -1,9 +1,13 @@
 import keyboard
+import sms
+
 while True:
     try:
         if keyboard.is_pressed('q'):
+            #sms.send("15186505491", "hello from the other pi")#quillan's phone #
             print("pressed q")
             break
+    
     except:
         print("didn't press q")
         break
