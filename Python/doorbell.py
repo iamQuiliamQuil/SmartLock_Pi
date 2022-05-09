b9cf70cc2497556e15c32dc349fb6d9cc64d6bcc
iamQuiliamQuil <quillanbrady@yahoo.com>
2022-05-09 19:43:52 -0400

    DING DONG

--- Python/doorbell.py
+++ Python/doorbell.py
@@ -1,12 +1,21 @@
 import hardware
 import sms
 import MyIP
+
+from os import environ
+environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
 import RPi.GPIO as GPIO
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load("/home/pi/Desktop/SmartLock_Pi/Storage/Sounds/doorbell-1.wav")
 
 def doorbell(channel):
-    #filename = hardware.capture("max","jpeg")
+    pygame.mixer.music.play()
+    filename = hardware.capture("max","jpeg")
     #myIP might not work
-    #sms.send_image("19146190386",filename,MyIP.get_ip(),"Someone is at your door!")
+    sms.send_image("19146190386",filename,MyIP.get_ip(),"Someone is at your door!")
 
 def toggleLock(channel):
     if hardware.__getFlag("locked") == False:
--- Python/sms_picture.sh
+++ Python/sms_picture.sh
@@ -3,6 +3,6 @@
 curl -XPOST https://api.twilio.com/2010-04-01/Accounts/AC5bea98d360df7e4d50af0fb6d8e34621/Messages.json \
 --data-urlencode "To=$1" \
 --data-urlencode "From=+19804093427" \
---data-urlencode "MediaUrl=http://$3:8001/Pictures/$2" \
+--data-urlencode "MediaUrl=http://$3:8001/Captures/$2" \
 --data-urlencode "Body=$4" \
 -u 'AC5bea98d360df7e4d50af0fb6d8e34621:284691e649e31c7afeae5fb9694d481d'
