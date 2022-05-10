dceb73d09e7f210f3b2f38ec9905ec7636ea4b0c
Eric <98417660+4ir-wick@users.noreply.github.com>
2022-05-10 13:56:47 -0400

    commnets!!!!!

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 
-string filename = "/usr/bin/sudo";
-string command = "python ../Lock_Control/lock.py";
+string filename = "/usr/bin/sudo"; //path with commands
+string command = "python ../Lock_Control/lock.py"; //run the command to execute the script
 Process commandExec = new Process();
 commandExec.StartInfo.FileName = filename;
 commandExec.StartInfo.Arguments = command;
-commandExec.StartInfo.UseShellExecute = true;
+commandExec.StartInfo.UseShellExecute = true; //tells the process to call a  command
 commandExec.Start();
