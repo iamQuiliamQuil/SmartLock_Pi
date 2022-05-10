1d3f1ebd56f9d5caced90132183e2840f251dc9d
Eric <98417660+4ir-wick@users.noreply.github.com>
2022-05-10 14:19:03 -0400

    c0ommenmtnes

--- Python/bluetooth_pi.py
+++ Python/bluetooth_pi.py
@@ -1,3 +1,7 @@
+# this is the python implementation of bluetooth
+# this most likely can work but wasn't tested
+# would theoretically run on the pi
+
 import bluetooth
 
 piAddress = "E4:5F:01:92:40:3F"
@@ -10,10 +14,10 @@ s.listen(backlog)
 try:
     app, address = s.accept()
     while 1:
-        data = app.recv(size)
+        data = app.recv(size) # recieve some stuff
         if data:
-            print(data)
-            client.send(data)
+            print(data) # print what it got
+            client.send(data) # send it back for verification on the app side of things
 except:
     print("Closing socket")
     client.close()
